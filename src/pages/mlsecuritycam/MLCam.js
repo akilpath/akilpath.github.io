e8bd412458a2560be2ae8dfeeb5793906f049466
akilpath <56412097+akilpath@users.noreply.github.com>
2023-06-24 02:09:49 -0400

    Added model one

--- src/pages/mlsecuritycam/MLCam.js
+++ src/pages/mlsecuritycam/MLCam.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProjectCSS from "./proj.module.css"
 
 import mainProjImg from "./securitycam.jpg"
+import modelOneData from "./training_accuracy.png"
 
 
 class MLCam extends React.Component{
@@ -25,9 +26,19 @@ class MLCam extends React.Component{
                     In the image above you can see simple assembly as an exploded view.
                 </p>
 
-                <p className={ProjectCSS.p}>Currently in this project, I've trained my first model on a dataset of over 900 images and achieved a 
-                validation accuracy of over 95%. I'm currently in the process of testing this model and collecting more images to improve the models
+                <p className={ProjectCSS.p}>Currently in this project, I've trained my first model on a dataset of over 1000 images and achieved a 
+                validation accuracy of over 97%. I'm currently in the process of testing this model and collecting more images to improve the models
                 performance in different times of day, weather and environments.</p>
+
+                <p className = {ProjectCSS.p}>
+                    The model uses a CNN that takes a 500x500 rgb image as an input. During training, a random brightness layer and random contrast layer is used
+                    to simulate different weather/environments. Dropout layers are also used to reduce overfitting.
+                </p>
+
+                <p className = {ProjectCSS.p}>
+                    Here is an image of the accuracy and loss during training.
+                </p>
+                <img className={ProjectCSS.Figure} src = {modelOneData}/>
 
                 <p className={ProjectCSS.p}>
                 The current code is available on my {" "} 
