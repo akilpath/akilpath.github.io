333a420e3dceb895ab570c2b48f78b6bab10b3ab
Akil Pathiranage <akilpath@example.com>
2026-05-14 11:43:05 -0400

    Updated website

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,163 +1,140 @@
 import React from 'react';
-import HomePageCSS from "./css/Homepage.module.css";
-import BriefAboutMeCSS from "./css/BriefAboutMe.module.css";
-import ProjectCSS from "./css/Project.module.css";
+import styles from './css/Homepage.module.css';
+import myphoto from './images/myphoto.jpg';
 
-import dunkirk from "./pages/Dunkirk/dunkirk.JPG";
-import legoSwerve from "./pages/projMagnemite/legoSwerve.jpg"
-import chessLogo from "./pages/chess/chesspluspluslogo.jpg";
-import binarySortVisLogo from "./images/binarysortvislogo.jpg";
-import securityCamImg from "./pages/mlsecuritycam/securitycam.jpg";
-import dinoGif from "./pages/dqn/dinogif.gif"
+const socialLinks = [
+  {
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/akilpath',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6C1.12 6 0 4.88 0 3.5C0 2.12 1.12 1 2.5 1C3.87 1 4.98 2.12 4.98 3.5ZM0.24 24H4.76V7.98H0.24V24ZM7.5 7.98H12V9.78H12.08C12.87 8.62 14.67 7.3 17.13 7.3C22.12 7.3 23 10.81 23 15.35V24H18.5V16.4C18.5 14.53 18.45 11.99 15.85 11.99C13.21 11.99 12.72 13.94 12.72 16.19V24H8.23V7.98H7.5Z" fill="currentColor"/>
+      </svg>
+    ),
+  },
+  {
+    label: 'GitHub',
+    href: 'https://github.com/akilpath',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 0.5C5.37 0.5 0 5.87 0 12.5C0 17.8 3.44 22.2 8.21 23.8C8.82 23.9 9.02 23.5 9.02 23.2C9.02 22.9 9 22.1 9 21.2C5.66 21.9 4.97 19.7 4.97 19.7C4.43 18.2 3.63 17.8 3.63 17.8C2.55 17.1 3.72 17.1 3.72 17.1C4.93 17.2 5.56 18.4 5.56 18.4C6.64 20.2 8.34 19.7 9.02 19.4C9.12 18.6 9.42 18.1 9.76 17.8C7.1 17.5 4.31 16.5 4.31 11.7C4.31 10.3 4.76 9.2 5.5 8.3C5.4 8 5.04 6.8 5.6 5.2C5.6 5.2 6.63 4.9 9 6.5C9.96 6.3 10.98 6.2 12 6.2C13.02 6.2 14.04 6.3 15 6.5C17.37 4.9 18.4 5.2 18.4 5.2C18.96 6.8 18.6 8 18.5 8.3C19.24 9.2 19.69 10.3 19.69 11.7C19.69 16.5 16.9 17.5 14.24 17.8C14.72 18.3 15.1 19.2 15.1 20.5C15.1 22.3 15.08 23.8 15.08 23.9C15.08 24.2 15.28 24.6 15.9 23.8C20.67 22.2 24.1 17.8 24.1 12.5C24.1 5.87 18.73 0.5 12.1 0.5H12Z" fill="currentColor"/>
+      </svg>
+    ),
+  },
+  {
+    label: 'Google Scholar',
+    href: 'https://scholar.google.com/citations?user=yFl0RqQAAAAJ&hl=en',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 3 2 8l10 5 10-5-10-5z" fill="currentColor" />
+        <path d="M6 9.5v8.5l6 3 6-3V9.5l-6 3-6-3z" fill="currentColor" />
+        <path d="M8.5 14.5h7v1h-7zM8.5 17.5h7v1h-7z" fill="currentColor" />
+      </svg>
+    ),
+  },
+];
 
-import myphoto from "./images/myphoto.png"
-
-import cpplogo from "./images/languageIcons/cpplogo.png";
-import pythonlogo from "./images/languageIcons/pythonlogo.png";
-import jslogo from "./images/languageIcons/javascriptlogo.png";
-import htmllogo from "./images/languageIcons/htmllogo.png";
-import csslogo from "./images/languageIcons/csslogo.png";
-import javalogo from "./images/languageIcons/javalogo.png";
-import tensorflowLogo from "./images/languageIcons/tensorflowlogo.png";
-import waterlooLogo from "./images/languageIcons/uwaterloo.logo.png"
-import solidworksLogo from "./images/languageIcons/solidworkslogo.png"
-import reactlogo from "./images/languageIcons/reactlogo.png"
-import firstLogo from "./images/languageIcons/FIRSTlogo.png"
-import gitlogo from "./images/languageIcons/gitlogo.png"
-
-import {Link} from "react-router-dom";
-
-function Homepage(){
+function Homepage() {
   return (
-    <div className={HomePageCSS.Homepage}>
-      <BriefAboutMe/>
-      <Proj
-          imagePath = {dinoGif} 
-          title = "DEEP Q DINO RUNNER" 
-          description = { `This Smart Security Camera uses an AI to detect whether someone is at my door or not, notifying me by text if there is. It uses a binary image classifier trained
-          using TensorFlow 2.0 and deployed on a RaspberryPi 3B+ through TensorFlow Lite. `}
-          link = "/DQN"
-          linkMessage = "See on GitHub"
-          left = {false}
-      />
-      <Proj
-          imagePath = {securityCamImg} 
-          title = "SMART CCTV" 
-          link = "/mlsecuritycam"
-          left = {true}
-      />
-      <Proj
-          imagePath = {legoSwerve}
-          title = "PROJECT MAGNEMITE"
-          link = "/swerve"
-          left = {false}
-      />
-      <Proj
-          imagePath = {dunkirk}
-          title = "DUNKIRK"
-          link = "/Dunkirk"
-          left = {true}
-      
-      />
-      <Proj 
-          imagePath = {chessLogo} 
-          title = "CHESS PLUS PLUS" 
-          link = "https://github.com/HDSB-GWS-Brooks-202122-ICS4-01/Chess-Plus-Plus"
-          left = {false}
-      />
-      <Proj
-          imagePath = {binarySortVisLogo} 
-          title = "BINARY SORT VISUALIZATION" 
-          link = "https://github.com/akilpath/Binary-Search-Tree-Visualization"
-          left = {true}
-      />
-    </div>
-  );
-}
-
-function BriefAboutMe(){
-  return (
-    <div className={BriefAboutMeCSS.BriefAboutMe}>
-      <div style = {{margin: "auto"}}className= {BriefAboutMeCSS.LogoContainer}>
-        <img className = {BriefAboutMeCSS.Icon} src = {waterlooLogo} alt = "" style = {{gridRow: "1", gridColumnStart: "1", gridColumnEnd: "3"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {cpplogo} alt = "" style = {{gridRow: "1", gridColumn: "3"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {pythonlogo} alt = "" style = {{gridRow: "1", gridColumn: "4"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {gitlogo} alt = "" style = {{gridColumnStart: "5", gridColumnEnd: "7"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {csslogo} alt = "" style = {{gridRow: "1", gridColumn: "7"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {javalogo} alt = "" style = {{gridRow: "1", gridColumn: "8"}}/>
-        
-        <img className = {BriefAboutMeCSS.Icon} src = {firstLogo} alt = "" style = {{gridColumnStart: "1", gridColumnEnd: "3"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {pythonlogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {jslogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {htmllogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {waterlooLogo} alt = "UW logo" style = {{gridColumnStart: "6", gridColumnEnd: "8"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {tensorflowLogo} alt = ""/>
-
-        <img className = {BriefAboutMeCSS.Icon} src = {cpplogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {pythonlogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {reactlogo} alt = "" style = {{gridColumnStart: "3", gridColumnEnd: "5"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {csslogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {javalogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {reactlogo} alt = "" style = {{gridColumnStart: "7", gridColumnEnd: "9"}}/>
-
-        <img className = {BriefAboutMeCSS.Icon} src = {pythonlogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {jslogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {htmllogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {firstLogo} alt = "" style = {{gridColumnStart: "4", gridColumnEnd: "6"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {tensorflowLogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {javalogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {cpplogo} alt = ""/>
-
-        <img className = {BriefAboutMeCSS.Icon} src = {solidworksLogo} alt = "" style = {{gridColumnStart: "1", gridColumnEnd: "3"}}/>
-        <img className = {BriefAboutMeCSS.Icon} src = {csslogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {javalogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {tensorflowLogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {javalogo} alt = ""/>
-        <img className = {BriefAboutMeCSS.Icon} src = {gitlogo} alt = "" style = {{gridColumnStart: "7", gridColumnEnd: "9"}}/>
-      </div>
-      <div className= {BriefAboutMeCSS.Name}>
-        <div>
-          <img className = {BriefAboutMeCSS.Photo} src = {myphoto} alt = ""/>
-          <h1>Akil Pathiranage</h1>
-          <p>
-            I'm a Mechatronics Engineering student @ UW with a passion for SWE and ML. 
+    <main className={styles.page}>
+      <header className={styles.header}>
+        <div className={styles.heroCopy}>
+          <p className={styles.kicker}>AI Enthusiast</p>
+          <h1 className={styles.title}>Akil Pathiranage</h1>
+          <p className={styles.description}>
+            Hi my name is Akil. I'm studying mechatronics at UW with a focus on AI. I'm passionate about ml with experience across NLP, computer vision, and robotics. I enjoy exploring how learning systems can solve real-world problems.
           </p>
+          <div className={styles.links}>
+            {socialLinks.map((link) => (
+              <a
+                key={link.label}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.iconButton}
+                aria-label={link.label}
+              >
+                {link.icon}
+              </a>
+            ))}
+          </div>
+        </div>
+        <img className={styles.photo} src={myphoto} alt="Akil Pathiranage" />
+      </header>
+
+      {/* <section className={styles.section}>
+        <div className={styles.card}>
+          <h2>About</h2>
           <p>
-            <b>Scroll to see some of my projects!</b>
+            I am a mechatronics engineer and researcher specializing in applied machine learning and embedded perception. I develop robust algorithms for autonomous agents that sense complex environments and act with confidence.
           </p>
         </div>
-      </div>
-    </div>
+      </section> */}
+
+      <section className={styles.gridSection}>
+        <div className={styles.card}>
+          <h2>Research areas</h2>
+          <ul className={styles.list}>
+            <li>Reinforcement Learning and OPD for LLMs</li>
+            <li>Neural Rendering and World Models</li>
+            <li>Diffusion Models</li>
+            <li>Robotics</li>
+          </ul>
+        </div>
+        <div className={styles.card}>
+          <h2>Experience</h2>
+          <div className={styles.bubbleGrid}>
+            <div className={styles.bubble}>
+              <strong>Cerebras</strong>
+              <p>Reasoning, RL and OPD for LLMs.</p>
+            </div>
+            <div className={styles.bubble}>
+              <strong>Waabi</strong>
+              <p>Sensor simulation for autonomous vehicles.</p>
+            </div>
+            <div className={styles.bubble}>
+              <strong>University of Waterloo</strong>
+              <p>4D Reconstruction and computer vision for food.</p>
+            </div>
+            <div className={styles.bubble}>
+              <strong>Fermilab</strong>
+              <p>Quantum networking across cities.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.card}>
+          <h2>Selected Articles</h2>
+          <ol className={styles.publications}>
+            <li>
+              <a href="https://www.cerebras.ai/blog/cepo-update-turbocharging-reasoning-models-capability-using-test-time-planning" target="_blank" rel="noopener noreferrer">
+                CePO Update: Turbocharging Reasoning Models’ capability using test-time planning
+              </a>
+              <span>• Cerebras blog post</span>
+            </li>
+            <li>
+              <a href="https://arxiv.org/pdf/2405.07121" target="_blank" rel="noopener noreferrer">
+                In The Wild Ellipse Parameter Estimation for Circular Dining Plates and Bowls
+              </a>
+              <span>• CVPR 2024 MetaFood Workshop</span>
+            </li>
+            <li>
+              <a href="https://arxiv.org/abs/2510.06536" target="_blank" rel="noopener noreferrer">
+                Optimal filtering and generation of entangled photons for quantum applications in the presence of noise
+              </a>
+              <span>• Accepted to Physical Review Applied</span>
+            </li>
+          </ol>
+        </div>
+      </section>
+
+      <footer className={styles.footer}>
+        <p>Feel free to reach out to me on LinkedIn!</p>
+      </footer>
+    </main>
   );
-}
-
-function Proj ({title, imagePath, link, left}){
-
-  if (left){
-    return (
-      <div className= {ProjectCSS.Banner}>
-        <div className = {ProjectCSS.descriptionContainer}>
-          <h2 className = {ProjectCSS.projectHeader}>{title}</h2>
-          <Link to = {link} className = {ProjectCSS.projButton}>View More </Link>
-        </div>
-        <div className= {ProjectCSS.imageContainer}>
-          <img className = {ProjectCSS.image} src = {imagePath} alt = "project" />
-        </div>
-    </div>
-    );
-  } else{
-    return (
-      <div className= {ProjectCSS.Banner}>
-          <div className= {ProjectCSS.imageContainer}>
-            <img className = {ProjectCSS.image} src = {imagePath} alt = "project" />
-          </div>
-          <div className = {ProjectCSS.descriptionContainer}>
-            <h2 className = {ProjectCSS.projectHeader}>{title}</h2>
-            <Link to = {link} className = {ProjectCSS.projButton}>View More </Link>
-          </div>
-      </div>
-    )
-  }
 }
 
 export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,68 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Homepage from './Homepage';
-
-import ProjectMagnemite from './pages/projMagnemite/ProjectMagnemite'
-import DQN from './pages/dqn/DQN'
-import MLCam from './pages/mlsecuritycam/MLCam'
-
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import NavCSS from './css/NavigationBar.module.css'
-import LinksCSS from "./css/Links.module.css";
-import linkedinLogo from "./images/linkedInLogo.png";
-import githubLogo from "./images/githubLogo.png";
-
-import {HashRouter, Routes, Route, Link} from "react-router-dom";
-import Dunkirk from './pages/Dunkirk/Dunkirk';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//testss
-
-function NavigationBar() {
-  return (
-    <div className = {NavCSS.Container}>
-        <Link to = "/" className={NavCSS.Link}>HOME</Link>
-        {/* <Link to = "/about" className = {NavCSS.Link}>ABOUT</Link> */}
-        <div className={NavCSS.ProjDropdown}>
-          PROJECTS
-          <div className={NavCSS.ProjDropdownContent}>
-            <Link to = "/DQN" className={NavCSS.ProjLink}>DEEP Q NETWORK</Link>
-            <Link to = "/swerve" className={NavCSS.ProjLink}>PROJECT MAGNEMITE</Link>
-            <Link to = "/mlsecuritycam" className={NavCSS.ProjLink}>ML SECURITY CAM</Link>
-            <Link to = "/Dunkirk" className={NavCSS.ProjLink}>DUNKIRK</Link>
-          </div>
-        </div>
-      </div>
-  )
-}
-
-function Links(){
-  return (
-    <div className = {LinksCSS.Links}>
-      <a href = "https://www.linkedin.com/in/akilpath" target = "_blank" rel = "noopener noreferrer"><img className = {LinksCSS.LinkImage} src = {linkedinLogo} alt = "linked in logo"/></a>
-      <a href = "https://github.com/akilpath" target = "_blank" rel = "noopener noreferrer"><img className = {LinksCSS.LinkImage} src = {githubLogo} alt = "github logo" /></a>
-    </div>
-  )
-}
 
 root.render(
   <React.StrictMode>
-      <HashRouter>
-        <NavigationBar />
-        <Routes>
-          <Route index path= "/" element = {<Homepage/>}/>
-          <Route path = "/swerve" element = {<ProjectMagnemite/>}/>
-          <Route path = "/DQN" element = {<DQN/>}/>
-          <Route path = "/mlsecuritycam" element = {<MLCam/>}/>
-          <Route path = "/Dunkirk" element = {<Dunkirk/>}/>
-        </Routes>
-        <Links/>
-      </HashRouter>
+    <Homepage />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
